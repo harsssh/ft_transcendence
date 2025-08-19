71ecf15eaee840af9d3651f3d84fc9c124a87960
smatsuodev <git@smatsuo.dev>
2025-08-19 17:54:20 +0900

    npm scriptにlint:codestyleとlint:codestyle:fixを追加して、biomeに準拠するようにコードを修正

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
-import Fastify from "fastify";
+import Fastify from 'fastify'
 
 const fastify = Fastify({
-  logger: true,
-});
+	logger: true,
+})
 
-fastify.get("/", async (req, res) => {
-  return { hello: "world" };
-});
+fastify.get('/', async (_req, _res) => {
+	return { hello: 'world' }
+})
 
 try {
-  await fastify.listen({ port: 3000 });
+	await fastify.listen({ port: 3000 })
 } catch (err) {
-  fastify.log.error(err);
-  process.exit(1);
+	fastify.log.error(err)
+	process.exit(1)
 }
