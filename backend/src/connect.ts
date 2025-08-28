3beb2a1140dc059a129510903f9e63a805957364
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-08-29 02:28:17 +0900

    packages/generated にファイルを生成 & server のビルドに tsup を使う

--- backend/src/connect.ts
+++ backend/src/connect.ts
@@ -1,9 +1,8 @@
 import type { ConnectRouter } from '@connectrpc/connect'
-import { HelloService } from './generated/connectrpc/hello/v1/hello_pb'
+import { HelloService } from '@workspace/generated/connectrpc/hello/v1/hello_pb'
 
 export default (router: ConnectRouter) =>
   router.service(HelloService, {
-    // implements rpc Say
     async say(req) {
       return {
         sentence: `You said: ${req.sentence}`,
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,18 +2,7 @@ import { fastifyConnectPlugin } from '@connectrpc/connect-fastify'
 import { fastify } from 'fastify'
 import routes from './connect'
 
-async function main() {
-  const server = fastify()
-  await server.register(fastifyConnectPlugin, {
-    routes,
-  })
-  server.get('/', (_, reply) => {
-    reply.type('text/plain')
-    reply.send('Hello World!')
-  })
-  await server.listen({ host: 'localhost', port: 8080 })
-  console.log('server is listening at', server.addresses())
-}
-// You can remove the main() wrapper if you set type: module in your package.json,
-// and update your tsconfig.json with target: es2017 and module: es2022.
-void main()
+const server = fastify()
+await server.register(fastifyConnectPlugin, { routes })
+await server.listen({ host: 'localhost', port: 3000 })
+console.log('server is listening at', server.addresses())
