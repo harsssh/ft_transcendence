afb26e047438a2e73f3b3b9cde21fbe4e968a630
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-08-30 21:51:49 +0900

    merge

--- frontend/app/index.ts
+++ frontend/app/index.ts
@@ -1,13 +1,10 @@
-import { client } from '../shared/api/client'
 import './style.css'
 
 document.addEventListener('DOMContentLoaded', async () => {
-  const res = await client.helloService.say({ sentence: 'hello' })
-
   document
     .querySelector('#app')
     ?.insertAdjacentHTML(
       'afterend',
-      `<h1 class="text-3xl underline">${res.sentence}</h1>`,
+      `<h1 class="text-3xl underline">some message</h1>`,
     )
 })
