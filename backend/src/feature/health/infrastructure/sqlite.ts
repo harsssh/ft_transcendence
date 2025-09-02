55f9c90ce4486ee13cb8fd074818e3b3ce337329
smatsuodev <git@smatsuo.dev>
2025-09-02 17:01:36 +0900

    SQLite のコネクションを使い回すように変更

--- backend/src/feature/health/infrastructure/sqlite.ts
+++ backend/src/feature/health/infrastructure/sqlite.ts
@@ -1,10 +1,12 @@
 import Database from 'better-sqlite3'
 import type { IHealthCheckPort } from '../application'
 
+const db = new Database(process.env.SQLITE_APP_DB_PATH)
+
 export const sqliteHealthCheckAdapter: IHealthCheckPort = {
   getStatus: async () => {
     try {
-      new Database(process.env.SQLITE_APP_DB_PATH)
+      db.prepare('SELECT 1').run()
       return 'healthy'
     } catch (e) {
       console.log(e)
