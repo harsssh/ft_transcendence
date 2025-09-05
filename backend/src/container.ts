189be6d0e796fadddbdb4894c5bb3e86b4dbab52
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-09-05 12:45:28 +0900

    format

--- backend/src/container.ts
+++ backend/src/container.ts
@@ -2,20 +2,17 @@ import * as awilix from 'awilix'
 import { sqliteHealthCheckAdapter } from './feature/health/infrastructure/sqlite.js'
 
 export const container = awilix.createContainer({
-    injectionMode: awilix.InjectionMode.CLASSIC,
-    strict: true,
+  injectionMode: awilix.InjectionMode.CLASSIC,
+  strict: true,
 })
 
-container.loadModules(
-    ['**/*.ts'],
-    {
-        formatName: 'camelCase',
-        resolverOptions: {
-            lifetime: awilix.Lifetime.SINGLETON,
-        }
-    }
-)
+container.loadModules(['**/*.ts'], {
+  formatName: 'camelCase',
+  resolverOptions: {
+    lifetime: awilix.Lifetime.SINGLETON,
+  },
+})
 
 container.register({
-    healthCheckTargets: awilix.asValue([sqliteHealthCheckAdapter])
+  healthCheckTargets: awilix.asValue([sqliteHealthCheckAdapter]),
 })
