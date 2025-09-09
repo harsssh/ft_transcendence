209c60e91de2d49dddb1f8a7b07c9aeb4c284a53
smatsuodev <git@smatsuo.dev>
2025-09-10 03:25:38 +0900

    tsup.config で awilix-types を実行できるようにutilityを追加

--- backend/tsup.config.ts
+++ backend/tsup.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'tsup'
+import { generateTypes } from '@workspace/type-safe-awilix/build'
+
+await generateTypes({ load: 'src/**/*.ts', outDir: 'src/generated' })
 
 export default defineConfig(() => ({
   entry: ['src/**/*.ts', '!src/**/*.test.ts'],
