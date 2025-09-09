49c73efe386f183dc136d404bf5ccfcd1d57949b
smatsuodev <git@smatsuo.dev>
2025-09-10 03:01:43 +0900

    fix format

--- backend/tsup.config.ts
+++ backend/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig(() => ({
-  entry: ['src/**/*.ts', "!src/**/*.test.ts"],
+  entry: ['src/**/*.ts', '!src/**/*.test.ts'],
   outDir: 'dist',
   format: ['esm'],
   platform: 'node',
