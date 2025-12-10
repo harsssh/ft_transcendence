f9472529631c05962e2b4fb2450ad5e5fcce33f2
smatsuodev <git@smatsuo.dev>
2025-12-10 12:45:40 +0900

    @mantine/coreと@mantine/hooksを導入

--- app/contexts/db.ts
+++ app/contexts/db.ts
@@ -1,5 +1,5 @@
-import { createContext } from 'react-router'
 import { drizzle } from 'drizzle-orm/node-postgres'
+import { createContext } from 'react-router'
 
 const db = drizzle(process.env.DATABASE_URL ?? '')
 
