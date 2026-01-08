27e5906f4a7ba29a39657a4723546954960d1cd2
smatsuodev <git@smatsuo.dev>
2026-01-09 03:14:11 +0900

    UserAvatarの必須パラメーターはdefault値を設定しなくて良い

--- app/routes/channels+/@me+/$channelId+/ui/UserAvatar.tsx
+++ app/routes/channels+/@me+/$channelId+/ui/UserAvatar.tsx
@@ -6,11 +6,7 @@ export type UserAvatarProps = {
   size?: number | MantineSize
 }
 
-export function UserAvatar({
-  name = null,
-  src = null,
-  size = 40,
-}: UserAvatarProps) {
+export function UserAvatar({ name, src, size = 40 }: UserAvatarProps) {
   return (
     <Avatar src={src} radius="50%" color="initials" size={size}>
       {name?.slice(0, 2).toUpperCase() ?? '??'}
