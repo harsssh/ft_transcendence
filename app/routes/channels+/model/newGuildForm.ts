1a336af2c850265ea9a8c17b2dbbaa2d05232837
Kazuki Kawamoto <kakawamo@c5r3s1.42tokyo.jp>
2026-01-08 20:40:39 +0900

    add validation rules to new guild form schema

--- app/routes/channels+/model/newGuildForm.ts
+++ app/routes/channels+/model/newGuildForm.ts
@@ -1,5 +1,9 @@
 import z from 'zod'
 
 export const NewGuildFormSchema = z.object({
-  name: z.string(),
+  name: z
+    .string({ required_error: 'Must be between 2 and 100 in length' })
+    .trim()
+    .min(2, 'Must be between 2 and 100 in length')
+    .max(100, 'Must be between 2 and 100 in length'),
 })
