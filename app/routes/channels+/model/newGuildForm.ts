8c8f33b7b3a424c1b26e3fff9766b2a15daa2d5d
Kazuki Kawamoto <kakawamo@c5r3s1.42tokyo.jp>
2026-01-08 21:14:40 +0900

    update zod schema for guild creation

--- app/routes/channels+/model/newGuildForm.ts
+++ app/routes/channels+/model/newGuildForm.ts
@@ -2,7 +2,7 @@ import z from 'zod'
 
 export const NewGuildFormSchema = z.object({
   name: z
-    .string({ required_error: 'Must be between 2 and 100 in length' })
+    .string()
     .trim()
     .min(2, 'Must be between 2 and 100 in length')
     .max(100, 'Must be between 2 and 100 in length'),
