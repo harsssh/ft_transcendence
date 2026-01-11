61b6a6bdaa7e0f06f0d9b64a9ce067a5a3ebad9c
smatsuodev <git@smatsuo.dev>
2026-01-11 15:13:19 +0900

    使わなくなったMINIO_ENDPOINTを削除

--- types/env.d.ts
+++ types/env.d.ts
@@ -4,7 +4,6 @@ declare module 'bun' {
     DATABASE_URL: string
     MINIO_ACCESS_KEY: string
     MINIO_SECRET_KEY: string
-    MINIO_ENDPOINT?: string
     STORAGE_PUBLIC_ENDPOINT?: string
     PRESENCE_DB_URL?: string
   }
